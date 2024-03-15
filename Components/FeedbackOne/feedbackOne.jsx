47db3eccefd3d105314ae0d4f28ef4aaa790f4c0
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-03-15 22:07:47 +0100

    2 feedback cards

--- Components/FeedbackOne/feedbackOne.jsx
+++ Components/FeedbackOne/feedbackOne.jsx
@@ -49,7 +49,7 @@ export default function FeedbackOne() {
             - клас!
           </p>
         </div>
-        <div>
+        <div className="imageClientLast">
           <Image src={client3} width={130} height={130} alt="Our clients" />
         </div>
       </div>
