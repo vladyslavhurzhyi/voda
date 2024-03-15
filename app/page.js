b7e40e72564ce58c14ac1cb407d10e457426dfce
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-03-15 17:58:39 +0100

    css and wave

--- app/page.js
+++ app/page.js
@@ -5,12 +5,14 @@ import Image from "next/image";
 
 import Hero from "@/Components/Hero/Hero";
 import FeedbackOne from "@/Components/FeedbackOne/feedbackOne";
+// import FeedbackSecond from "@/Components/FeedbackSecond/feedbackSecond";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
       <FeedbackOne />
+      {/* <FeedbackSecond /> */}
       <Faq />
     </>
   );
