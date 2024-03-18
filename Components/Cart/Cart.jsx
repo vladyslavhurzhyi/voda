62fa06c16eefcaf8bb96149e625504f2273d1315
vladyslavhurzhyi <soulline0@gmail.com>
2024-03-18 23:42:27 +0100

    fix cart

--- Components/Cart/Cart.jsx
+++ Components/Cart/Cart.jsx
@@ -1,24 +1,53 @@
 "use client";
-import { useState } from "react";
+
 import CartFinalPrice from "../CartFinalPrice/CartFinalPrice";
 import CartList from "../CartList/CartList";
 import { useContext } from "react";
 import CartContext from "@/app/context/CartContext";
+import Link from "next/link";
+import Button from "../Button/Button";
 
 const Cart = () => {
   const { cart } = useContext(CartContext);
 
   return (
     <>
-      <div className="ml-[72px] mr-[72px] h-screen">
-        <h2 className=" font-bold text-[60px] text-[#F5821E] my-[60px] text-center">
-          Кошик
-        </h2>
-        <div className="flex justify-between">
-          <CartList cart={cart} />
-          <CartFinalPrice cart={cart} />
-        </div>
-      </div>
+      {cart.length < 1 ? (
+        <>
+          <div className=" ml-auto mr-auto max-w-[800px]">
+            <div className="flex flex-col items-center justify-center bg-white w-[749px] h-[658px]">
+              <p className="text-[#B3CBDB] text-[24px] mb-[24px]">
+                Кошик порожній
+              </p>
+              <p className="text-[#B3CBDB] text-[20px] mb-[60px]">
+                але то легко виправити :)
+              </p>
+
+              <Link href={"/"} className="flex">
+                <Button
+                  text={"Замовити"}
+                  className="px-[77px] py-[18px] flex items-center "
+                />
+              </Link>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className=" max-w-[1440px] mx-[72px] h-screen">
+            <div className=" ml-[72px] mr-[72px] ">
+              <h2 className=" font-bold text-[60px] text-[#F5821E] my-[60px] text-center">
+                Кошик
+              </h2>
+            </div>
+
+            <div className="flex justify-between">
+              <CartList cart={cart} />
+              <CartFinalPrice cart={cart} />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
