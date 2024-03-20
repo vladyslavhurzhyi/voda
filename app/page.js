e2fd2cdd2f9ae0bbf9979365f6311eb9f19349f2
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-03-20 17:03:45 +0100

    section Best

--- app/page.js
+++ app/page.js
@@ -1,27 +1,20 @@
-import Button from "@/Components/Button/Button";
-
-import Faq from "@/Components/Faq/Faq";
-
 import Hero from "@/Components/Hero/Hero";
-
-import Slider from "@/Components/Slider/Slider";
+import { Best } from "@/Components/Best/Best";
 import { ChomuZdorova } from "@/Components/ChomuZdorova/ChomuZdorova";
-import { SafeWater } from "@/Components/SafeWater/SafeWater";
 import { ChemicalComposition } from "@/Components/ChemicalComposition/ChemicalComposition";
+import { SafeWater } from "@/Components/SafeWater/SafeWater";
+import Slider from "@/Components/Slider/Slider";
+import Faq from "@/Components/Faq/Faq";
 
 export default function Home() {
   return (
     <>
       <Hero />
-
-      <SafeWater />
-
-      <Slider />
-
+      <Best />
       <ChomuZdorova />
-
       <ChemicalComposition />
-
+      <SafeWater />
+      <Slider />
       <Faq />
     </>
   );
