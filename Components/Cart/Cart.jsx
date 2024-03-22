4bee2bba1cfd0e8fb619465a77d403f9d8d8bcaf
vladyslavhurzhyi <soulline0@gmail.com>
2024-03-22 18:27:18 +0100

    new client action

--- Components/Cart/Cart.jsx
+++ Components/Cart/Cart.jsx
@@ -2,20 +2,28 @@
 
 import CartFinalPrice from "../CartFinalPrice/CartFinalPrice";
 import CartList from "../CartList/CartList";
-import { useContext } from "react";
+import { useState } from "react";
 
 import Link from "next/link";
 import Button from "../Button/Button";
 import { useCartStore } from "@/app/zustand/cartState/cartState";
+import { NewClientCheckBox } from "./newClientCheckBox";
+import Image from "next/image";
 
 const Cart = () => {
   const cart = useCartStore((state) => state.cartItems);
+
+  const [newClient, setNewClient] = useState(false);
+
+  const toggleNewClient = () => {
+    setNewClient((prevState) => !prevState);
+  };
 
   return (
     <>
       {cart.length < 1 ? (
         <>
-          <div className=" ml-auto mr-auto max-w-[800px]">
+          <div className=" mx-auto max-w-[800px]">
             <div className="flex flex-col items-center justify-center bg-white w-[749px] h-[658px]">
               <p className="text-[#B3CBDB] text-[24px] mb-[24px]">
                 Кошик порожній
@@ -35,16 +43,78 @@ const Cart = () => {
         </>
       ) : (
         <>
-          <div className=" max-w-[1440px] mx-[72px] ">
+          <div className="mx-auto max-w-[1440px] ">
             <div className=" ml-[72px] mr-[72px] ">
               <h2 className=" font-bold text-[60px] text-[#F5821E] my-[60px] text-center">
                 Кошик
               </h2>
             </div>
-
             <div className="flex justify-between">
               <CartList cart={cart} />
               <CartFinalPrice cart={cart} />
+            </div>
+
+            <div>
+              <NewClientCheckBox
+                newClient={newClient}
+                toggleNewClient={toggleNewClient}
+              />
+            </div>
+
+            <div className="pt-[24px]">
+              <div className=" w-[859px] h-36 bg-sky-500 rounded-[14px] justify-start items-center gap-4 inline-flex">
+                <div className="flex items-center justify-center text-white w-[200px] h-full bg-[#005087] rounded-l-[14px]">
+                  <p className=" text-[14px] px-0 ">Активована</p>
+                </div>
+
+                <div className=" text-white ml-[40px] my-[28px]">
+                  <p className="text-[24px]">Перше знайомство</p>
+                  <p className="text-[16px]">
+                    Купуйте два бутля води по ціні одного при першому замовленні
+                    якщо маєте тару на обмін.
+                  </p>
+                </div>
+
+                <div className="relative">
+                  <Image
+                    alt="action1"
+                    width={200}
+                    height={150}
+                    src="/newClientAction1.png"
+                  ></Image>
+                  <p className=" rotate-[30deg] z-10 right-0 top-2 uppercase font-semibold  absolute text-white text-[18px]">
+                    акція
+                  </p>
+                </div>
+              </div>
+
+              {/* /////action 2 */}
+
+              <div className=" mt-[24px] w-[859px] h-36 bg-sky-500 rounded-[14px] justify-start items-center gap-4 inline-flex ">
+                <div className="flex items-center justify-center text-white w-[200px] h-full bg-[#005087] rounded-l-[14px]">
+                  <p className=" text-[14px] ">Не активна</p>
+                </div>
+
+                <div className=" text-white ml-[40px] my-[28px]">
+                  <p className="text-[24px]">новий клієнт</p>
+                  <p className="text-[16px]">
+                    Замовляйте три полікарбонатних бутля з водою і отримайте
+                    механічну помпу в подарунок.
+                  </p>
+                </div>
+
+                <div className="relative">
+                  <Image
+                    alt="action1"
+                    width={186}
+                    height={144}
+                    src="/newClientAction2.png"
+                  ></Image>
+                  <p className=" rotate-[30deg] z-10 right-0 top-2 uppercase font-semibold  absolute text-white text-[18px]">
+                    акція
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </>
---
+++ Components/Cart/newClientCheckBox.jsx
@@ -0,0 +1,81 @@
+export const NewClientCheckBox = ({ toggleNewClient, newClient }) => {
+  return (
+    <>
+      <div className="flex items-center border-b-[1px] pb-[24px]">
+        <p>Ви новий клієнт?</p>
+
+        <div className="inline-flex items-center">
+          <label
+            className="relative flex items-center p-3 rounded-full cursor-pointer"
+            htmlFor="on"
+            data-ripple-dark="true"
+          >
+            <input
+              onChange={() => {
+                toggleNewClient();
+              }}
+              name="type"
+              checked={newClient ? true : false}
+              type="radio"
+              className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border-2 border-gray-300 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-[#91C81E]  hover:before:opacity-10"
+              id="morning"
+            />
+            <span className="absolute text-[#91C81E] transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3.5 w-3.5"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+              >
+                <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+              </svg>
+            </span>
+          </label>
+          <label
+            className={`${
+              newClient ? "text-[#91C81E]" : "text-gray-700"
+            } mt-px font-light  cursor-pointer select-none`}
+            htmlFor="morning"
+          >
+            Так
+          </label>
+        </div>
+
+        <div className="inline-flex items-center">
+          <label
+            className="relative flex items-center p-3 rounded-full cursor-pointer"
+            htmlFor="on"
+            data-ripple-dark="true"
+          >
+            <input
+              onChange={toggleNewClient}
+              name="type"
+              checked={newClient ? false : true}
+              type="radio"
+              className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border-2 border-gray-300 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-[#91C81E]  hover:before:opacity-10"
+              id="morning"
+            />
+            <span className="absolute text-[#91C81E] transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3.5 w-3.5"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+              >
+                <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+              </svg>
+            </span>
+          </label>
+          <label
+            className={`${
+              !newClient ? "text-[#91C81E]" : "text-gray-700"
+            } mt-px font-light  cursor-pointer select-none`}
+            htmlFor="morning"
+          >
+            Ні
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
