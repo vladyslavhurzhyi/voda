aec90fed24737b175dd396da55580f3c58988086
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-04-16 22:28:22 +0200

    mobileCatalogWater

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <>
       <CatalogWater />
-      <Hero />
+      {/* <Hero /> */}
       {/* <FormForOder />
       <SliderFirstMeet />
       <Steps />
