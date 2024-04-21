636818372afbcf218c04efcdb19db8ddf360ea96
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-04-21 20:15:17 +0200

    mobile

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
       {/* <FormForOder /> */}
       {/* <SliderFirstMeet /> */}
       {/* <Steps /> */}
-      {/* <Best /> */}
+      <Best />
       {/* <ChomuZdorova /> */}
       {/* <ChemicalComposition /> */}
       {/* <SafeWater /> */}
