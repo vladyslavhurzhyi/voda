a88ace68ef1dd2ce768e67fbc57869689d0cf131
Vladyslav Hurzhyi <80459886+vladyslavhurzhyi@users.noreply.github.com>
2024-06-01 18:12:11 +0200

    Update SliderFeedback.jsx

--- Components/SliderFeedback/SliderFeedback.jsx
+++ Components/SliderFeedback/SliderFeedback.jsx
@@ -5,7 +5,6 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { FeedbackOne } from "@/Components/FeedbackOne/feedbackOne";
 import { FeedbackSecond } from "@/Components/FeedbackSecond/FeedbackSecond";
-
 import { SlideNextButtonReview } from "./nextSwipeReview";
 import { SlidePrevButtonReview } from "./prevSwipeReview";
 import { FeedbackThree } from "../FeedbackThree/FeedbackThree";
@@ -14,18 +13,22 @@ import { FeedbackFive } from "../FeedbackFive/FeedbackFive";
 import { useState, useEffect } from "react";
 
 const SliderFeedback = () => {
+  const [isWideScreen, setIsWideScreen] = useState(false);
 
-  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1024);
-
- useEffect(() => {
+  useEffect(() => {
+    // Проверка наличия объекта window
     if (typeof window !== "undefined") {
       const handleResize = () => {
         setIsWideScreen(window.innerWidth >= 1024);
       };
 
+      // Изначальная установка значения
+      handleResize();
+
+      // Добавление слушателя события изменения размера окна
       window.addEventListener("resize", handleResize);
 
-      // Функция очистки для удаления слушателя событий
+      // Очистка слушателя при размонтировании компонента
       return () => {
         window.removeEventListener("resize", handleResize);
       };
@@ -33,57 +36,48 @@ const SliderFeedback = () => {
   }, []);
 
   return (
-    <>
-      <div className="relative bg-[#00AFF0]" id="reviews">
-        {" "}
-        <div className=" lg:max-w-[1440px] mx-auto">
-          <div className=" w-full absolute z-10 top-[6%] lg:top-[15%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-            {" "}
-            <h2 className="titleFeedback">
-              ВІДГУКИ НАШИХ <br /> КЛІЄНТІВ
-            </h2>
-          </div>
-          <div className=" relative  ">
-            <Swiper
-              loop={true}
-              modules={[Navigation]}
-              spaceBetween={0}
-              slidesPerView={1}
-            >
-              <SwiperSlide>
-                {isWideScreen && <SlideNextButtonReview type={"first"} />}
-                <FeedbackOne />
-                {isWideScreen && <SlidePrevButtonReview type={"first"} />}
-              </SwiperSlide>
+    <div className="relative bg-[#00AFF0]" id="reviews">
+      <div className="lg:max-w-[1440px] mx-auto">
+        <div className="w-full absolute z-10 top-[6%] lg:top-[15%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <h2 className="titleFeedback">
+            ВІДГУКИ НАШИХ <br /> КЛІЄНТІВ
+          </h2>
+        </div>
+        <div className="relative">
+          <Swiper loop={true} modules={[Navigation]} spaceBetween={0} slidesPerView={1}>
+            <SwiperSlide>
+              {isWideScreen && <SlideNextButtonReview type={"first"} />}
+              <FeedbackOne />
+              {isWideScreen && <SlidePrevButtonReview type={"first"} />}
+            </SwiperSlide>
 
-              <SwiperSlide>
-                {isWideScreen && <SlideNextButtonReview type={"second"} />}
-                <FeedbackSecond />
-                {isWideScreen && <SlidePrevButtonReview type={"second"} />}
-              </SwiperSlide>
+            <SwiperSlide>
+              {isWideScreen && <SlideNextButtonReview type={"second"} />}
+              <FeedbackSecond />
+              {isWideScreen && <SlidePrevButtonReview type={"second"} />}
+            </SwiperSlide>
 
-              <SwiperSlide>
-                {isWideScreen && <SlideNextButtonReview type={"third"} />}
-                <FeedbackThree />
-                {isWideScreen && <SlidePrevButtonReview type={"third"} />}
-              </SwiperSlide>
+            <SwiperSlide>
+              {isWideScreen && <SlideNextButtonReview type={"third"} />}
+              <FeedbackThree />
+              {isWideScreen && <SlidePrevButtonReview type={"third"} />}
+            </SwiperSlide>
 
-              <SwiperSlide>
-                {isWideScreen && <SlideNextButtonReview type={"fourth"} />}
-                <FeedbackFour />
-                {isWideScreen && <SlidePrevButtonReview type={"fourth"} />}
-              </SwiperSlide>
+            <SwiperSlide>
+              {isWideScreen && <SlideNextButtonReview type={"fourth"} />}
+              <FeedbackFour />
+              {isWideScreen && <SlidePrevButtonReview type={"fourth"} />}
+            </SwiperSlide>
 
-              <SwiperSlide>
-                {isWideScreen && <SlideNextButtonReview type={"fifth"} />}
-                <FeedbackFive />
-                {isWideScreen && <SlidePrevButtonReview type={"fifth"} />}
-              </SwiperSlide>
-            </Swiper>
-          </div>
+            <SwiperSlide>
+              {isWideScreen && <SlideNextButtonReview type={"fifth"} />}
+              <FeedbackFive />
+              {isWideScreen && <SlidePrevButtonReview type={"fifth"} />}
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
