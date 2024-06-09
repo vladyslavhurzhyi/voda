7e770ea6c553e70adc69a8e82e7d9b3fe8435c39
vladyslavhurzhyi <soulline0@gmail.com>
2024-06-09 13:57:05 +0200

    fix sendMessageFromFooter

--- app/utils/api/telegramFormFooter.jsx
+++ app/utils/api/telegramFormFooter.jsx
@@ -1,23 +1,14 @@
-const baseUrl =
-  "https://api.telegram.org/bot7017177531:AAHvpOLQOzTZScs41GpRFoNlcDLATAW0U4c/";
+const sendMessageFromFooter = async ({ name, phoneNumber }) => {
+  const response = await fetch("/api/sendMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, phoneNumber }),
+  });
 
-const sendMessageFromFooter = async ({ name, phoneNumber, comments }) => {
-  const telegramMessage = `
-  <b>Заказали звонок!</b>
-  
-  <b>Имя:</b> ${name}
-  <b>Телефон:</b> ${phoneNumber}
-  <b>Комментарий:</b> ${comments}
-  
-`;
-
-  const url = `${baseUrl}sendMessage?chat_id=-4135041896&text=${encodeURIComponent(
-    telegramMessage
-  )}&parse_mode=html`;
-
-  const response = await fetch(url);
-
-  console.log("response", response);
+  const data = await response.json();
+  console.log("response", data);
 };
 
 export default sendMessageFromFooter;
