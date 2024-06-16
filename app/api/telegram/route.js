87c752054bbdf8862d40501780ec90ffe0381d7f
vladyslavhurzhyi <soulline0@gmail.com>
2024-06-16 17:35:03 +0200

    fix date telegram api

--- app/api/telegram/route.js
+++ app/api/telegram/route.js
@@ -88,12 +88,7 @@ export async function POST(req) {
   <b>Дом:</b> ${house}
   <b>Корпус:</b> ${courpus}
   <b>Квартира:</b> ${apartment}
-  <b>Дата доставки:</b> ${new Date(deliveryDate).toLocaleDateString("uk-UA", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })}
+  <b>Дата доставки:</b> ${deliveryDate}
   <b>Время доставки:</b> ${
     deliveryTime === "morning" ? "9:00 - 12:00" : "18:00 - 21:00"
   }
