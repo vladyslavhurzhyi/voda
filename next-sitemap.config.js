bd733d35c8a2ace6a29f70d4d9699c34c65344c0
Vladyslav Hurzhyi <soulline0@gmail.com>
2024-07-21 18:51:04 +0200

    edit site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://voda-aquatica.od.ua/', // Replace with your site's URL
+    generateRobotsTxt: true, // (Optional) Generate a robots.txt file
+    // ...other options
+  }
+  
