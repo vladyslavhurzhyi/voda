61f16d0d14475717384454b3a28bb58bd75a519c
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2024-09-15 15:47:53 +0200

    title and description

--- app/bottle-cover/page.js
+++ app/bottle-cover/page.js
@@ -1,8 +1,19 @@
 import { BottleCover } from "@/Components/BottleCover/BottleCover";
+import Head from "next/head";
 
 export default function bottleCover() {
   return (
     <>
+      <Head>
+        <title>Чохли для бутлів</title>
+        <meta
+          name="Чохли для бутлів з питної води 19 літрів: захист та стиль для
+води Котовського, Фонтанка, Крижанівка, Поскот"
+          content="Замовляйте воду в бутлях та чохли для бутлів
+19 літрів у Котовського, Фонтанка, Крижанівка, Поскот +38(096)883 66 88. Захистіть свої бутлі від пилу та сонячних променів,
+додайте стиль до інтер'єру."
+        />
+      </Head>
       <BottleCover />
     </>
   );
