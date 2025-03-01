64fd1e2a2f630ecd3871f75905eb2f748e0630d3
Vladyslav Hurzhyi <soulline0@gmail.com>
2025-03-01 15:59:59 +0100

    fix

--- app/utils/calculateDiscountFinalPrice.js
+++ app/utils/calculateDiscountFinalPrice.js
@@ -3,7 +3,7 @@ import { calculateDiscountPrice, calculateTotalPrice } from "./reduceCalc";
 export const calculateFinalPrice = (cart, taraQuantity, actionDiscount) => {
   let finalPrice;
 
-  const tara = taraQuantity * 370;
+  const tara = taraQuantity * 380;
   finalPrice =
     calculateTotalPrice(cart) -
     calculateDiscountPrice(cart, actionDiscount) +
--- app/utils/calculateWaterPrice.js
+++ app/utils/calculateWaterPrice.js
@@ -16,13 +16,13 @@ export const calculateOnWaterPagePrice = (waterQuantity, waterType) => {
   let bottlePrice;
   if (waterQuantity >= 10) {
     // pricePerBottle = waterType === "mineralWater" ? 70 : 60;
-    bottlePrice = waterType === "mineralWater" ? 85 : 75;
+    bottlePrice = waterType === "mineralWater" ? 100 : 95;
   } else if (waterQuantity >= 6 && waterQuantity <= 9) {
     // pricePerBottle = waterType === "mineralWater" ? 80 : 65;
-    bottlePrice = waterType === "mineralWater" ? 95 : 85;
+    bottlePrice = waterType === "mineralWater" ? 110 : 100;
   } else if (waterQuantity >= 2 && waterQuantity <= 5) {
     // pricePerBottle = waterType === "mineralWater" ? 90 : 75;
-    bottlePrice = waterType === "mineralWater" ? 105 : 95;
+    bottlePrice = waterType === "mineralWater" ? 120 : 110;
   } else {
     // pricePerBottle = priceForWater; // для одной бутылки без скидки
     bottlePrice = waterType === "mineralWater" ? 140 : 130;
